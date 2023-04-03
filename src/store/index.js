29f8e54b5dead4fd8a329c8c037e44e9ec1f7270
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-04-03 19:16:10 +0530

    random color on reload

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,17 @@
 import { proxy } from 'valtio';
 
+const randomColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
 const state = proxy({
   intro: true,
-  color: '#EFBD48',
+  color: randomColor(),
   isLogoTexture: true,
   isFullTexture: false,
   logoDecal: './threejs.png',
