cd6d069bf2e8c3e60c9d714d82ff624f600c566a
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-04-03 19:16:27 +0530

    color picker

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { useSnapshot } from 'valtio';
+import state from '../store';
+import { SketchPicker } from 'react-color';
+import { color } from 'framer-motion';
 
 const ColorPicker = () => {
-  return <div>ColorPicker</div>;
+  const snap = useSnapshot(state);
+
+  return (
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        onChange={(color) => {
+          state.color = color.hex;
+        }}
+        presetColors={[
+          '#FFFFFF',
+          '#000000',
+          '#FF0000',
+          '#0066CC',
+          '#FFA500',
+          '#00FF00',
+          '#FFFF00',
+          '#800080',
+          '#FFC0CB',
+          '#808080',
+        ]}
+      />
+    </div>
+  );
 };
 
 export default ColorPicker;
